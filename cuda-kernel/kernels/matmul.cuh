d3702ca3aca17674f85a3fc293203c32ee3f68b2
minghuaw <michael.wu1107@gmail.com>
2023-11-14 19:14:47 -0800

    padded 2d block tiling improve perf a lot

--- cuda-kernel/kernels/matmul.cuh
+++ cuda-kernel/kernels/matmul.cuh
@@ -5,6 +5,22 @@
 #ifndef CUDA_KERNEL_MATMUL_CUH
 #define CUDA_KERNEL_MATMUL_CUH
 
+template<
+        const unsigned int TM,
+        const unsigned int TN
+>
+__device__ void compute_fragment(
+        const float *a_frag,
+        const float *b_frag,
+        float *c_frag
+) {
+    for (int i = 0; i < TM; i++) {
+        for (int j = 0; j < TN; j++) {
+            c_frag[i * TN + j] += a_frag[i] * b_frag[j];
+        }
+    }
+}
+
 namespace matmul {
     __global__ void hello() {
         unsigned int col = blockIdx.x * blockDim.x + threadIdx.x;
@@ -50,8 +66,8 @@ namespace matmul {
             float *matrix_c
     ) {
         // TODO: This can be passed in as a kernel argument
-        __shared__ float shared_a[BM * BK];
-        __shared__ float shared_b[BK * BN];
+        __shared__ float shared_a[BM * (BK + 1)];
+        __shared__ float shared_b[BK * (BN + 1)];
 
         unsigned int global_a_offset = blockIdx.y * BM * K;
         unsigned int global_b_offset = blockIdx.x * BN;
@@ -63,13 +79,13 @@ namespace matmul {
 
         float sum = 0.0;
         for (unsigned int k = 0; k < K; k += BK) {
-            shared_a[local_y * BK + local_x] = matrix_a[global_a_offset + local_y * K + k + local_x];
-            shared_b[local_y * BN + local_x] = matrix_b[global_b_offset + (local_y + k) * N + local_x];
+            shared_a[local_y * (BK + 1) + local_x] = matrix_a[global_a_offset + local_y * K + k + local_x];
+            shared_b[local_y * (BN + 1) + local_x] = matrix_b[global_b_offset + (local_y + k) * N + local_x];
 
             __syncthreads();
 
             for (unsigned int i = 0; i < BK; i++) {
-                sum += shared_a[local_y * BK + i] * shared_b[i * BN + local_x];
+                sum += shared_a[local_y * (BK + 1) + i] * shared_b[i * (BN + 1) + local_x];
             }
             __syncthreads();
         }
@@ -225,6 +241,85 @@ namespace matmul {
         }
     }
 
+    template<
+            const unsigned int M,
+            const unsigned int N,
+            const unsigned int K,
+            const unsigned int BM,
+            const unsigned int BN,
+            const unsigned int BK,
+            const unsigned int TM,
+            const unsigned int TN
+    >
+    __global__ void padded_block_tiling_2d(
+            const float alpha,
+            const float *A,
+            const float *B,
+            const float beta,
+            float *C
+    ) {
+//        unsigned int thread_index = threadIdx.y * blockDim.x + threadIdx.x;
+
+        unsigned int thread_index = threadIdx.x;
+        int block_row_thread = BN / TN;
+        int block_col_thread = BM / TM;
+        int thread_num = block_row_thread * block_col_thread; // 一个线程负责计算block中TM*TN个元素
+
+        int tx = (thread_index % block_row_thread) * TN;
+//        int tx = thread_index % BN; // This is wrong
+        int ty = (thread_index / block_row_thread) * TM;
+
+        __shared__ float As[BM * (BK + 1)];
+        __shared__ float Bs[BK * (BN + 1)];
+
+        // 移动到当前block
+        unsigned int a_global_offset = blockIdx.y * BM * K;
+        unsigned int b_global_offset = blockIdx.x * BN;
+        const unsigned int c_global_offset = blockIdx.y * BM * N + blockIdx.x * BN;
+
+        /*
+        当前线程负责搬运全局内存中第a_tile_row行，第a_tile_col列元素至共享内存第a_tile_row行，第a_tile_col列
+        a_tile_stride表示block中线程可搬运a_tile_stride行至共享内存；
+
+        若BM=64,BK=8,thread_num=512,则a_tile_stride=64,a_tile_stride=BM，表示每个线程搬运一轮即可完成所需元素的搬运;
+        若BM=128,BK=8,thread_num=512,则a_tile_stride=64,表示每个线程搬运两轮即可完成所需元素的搬运;
+        */
+        int a_tile_row = threadIdx.x / BK;
+        int a_tile_col = threadIdx.x % BK;
+        int a_tile_stride = thread_num / BK;
+
+        int b_tile_row = threadIdx.x / BN;
+        int b_tile_col = threadIdx.x % BN;
+        int b_tile_stride = thread_num / BN;
+
+//        float tmp[TM][TN] = {0.}; // 每个线程负责TM*TN个元素，则需要申请TM*TN个寄存器保存累加值，额外的一个寄存器用于缓存；
+        float c_tile[TM * TN] = {0.0};
+        for (int k = 0; k < K; k += BK) {
+            for (int i = 0; i < BM; i += a_tile_stride) {
+                As[(a_tile_row + i) * (BK + 1) + a_tile_col] = A[a_global_offset + (a_tile_row + i) * K + a_tile_col];
+            }
+            for (int i = 0; i < BK; i += b_tile_stride) {
+                Bs[(b_tile_row + i) * (BN + 1) + b_tile_col] = B[b_global_offset + (b_tile_row + i) * N + b_tile_col];
+            }
+            __syncthreads();
+            a_global_offset += BK;
+            b_global_offset += BK * N;
+
+            for (int kk = 0; kk < BK; kk++) {
+                for (int i = 0; i < TM; i++) {
+                    for (int j = 0; j < TN; j++)
+                        c_tile[i * TN + j] += As[(ty + i) * (BK + 1) + kk] * Bs[tx + j + kk * (BN + 1)];
+                }
+            }
+            __syncthreads();
+        }
+        for (int i = 0; i < TM; i++) {
+            for (int j = 0; j < TN; j++)
+                C[c_global_offset + (ty + i) * N + tx + j] =
+                        alpha * c_tile[i * TN + j] + beta * C[c_global_offset + (ty + i) * N + tx + j];
+        }
+    }
+
 #define OFFSET(row, col, ld) ((row)*(ld)+(col))
 #define FETCH_FLOAT4(pointer) (reinterpret_cast<float4*>(&(pointer))[0])
 
@@ -268,7 +363,8 @@ namespace matmul {
         int b_tile_col = threadIdx.x % (BN / 4) * 4;
         int b_tile_stride = BK / ldg_b_num; // 一共BK行，搬运ldg_b_num轮，每论搬运b_tile_stride行
 
-        float accum[TM][TN] = {0.}; // 每个线程负责TM*TN个元素，则需要申请TM*TN个寄存器保存累加值，额外的一个寄存器用于缓存；
+//        float accum[TM][TN] = {0.}; // 每个线程负责TM*TN个元素，则需要申请TM*TN个寄存器保存累加值，额外的一个寄存器用于缓存；
+        float c_frag[TM * TN] = {0.};
 
         // 计算ldg_a_num的所有参数必须全部是const，否则不能用来申明数组大小
         float ldg_a_reg[4 * ldg_a_num] = {0.}; // 每个线程搬运ldg_a_num轮，寄存器缓存ldg_a_num个float4元素，用于转置As矩阵
@@ -306,11 +402,13 @@ namespace matmul {
                 for (int n = 0; n < TN; n += 4) {
                     FETCH_FLOAT4(b_frag[n]) = FETCH_FLOAT4(Bs[OFFSET(i, tx + n, BN)]); // 偏移到当前thread tile
                 }
-                for (int m = 0; m < TM; m++) {
-                    for (int n = 0; n < TN; n++) {
-                        accum[m][n] += a_frag[m] * b_frag[n];
-                    }
-                }
+//                for (int m = 0; m < TM; m++) {
+//                    for (int n = 0; n < TN; n++) {
+////                        accum[m][n] += a_frag[m] * b_frag[n];
+//                        accum[m * TN + n] += a_frag[m] * b_frag[n];
+//                    }
+//                }
+                compute_fragment<TM, TN>(a_frag, b_frag, c_frag);
             }
             __syncthreads();
         }
@@ -318,48 +416,37 @@ namespace matmul {
             for (int n = 0; n < TN; n += 4) {
                 float4 ctmp = FETCH_FLOAT4(C[OFFSET(ty + m, tx + n, N)]);
                 //float4 atmp = FETCH_FLOAT4(accum[m][n]);
-                ctmp.x = alpha * accum[m][n] + beta * ctmp.x;
-                ctmp.y = alpha * accum[m][n + 1] + beta * ctmp.y;
-                ctmp.z = alpha * accum[m][n + 2] + beta * ctmp.z;
-                ctmp.w = alpha * accum[m][n + 3] + beta * ctmp.w;
+                // ctmp.x = alpha * accum[m][n] + beta * ctmp.x;
+                // ctmp.y = alpha * accum[m][n + 1] + beta * ctmp.y;
+                // ctmp.z = alpha * accum[m][n + 2] + beta * ctmp.z;
+                // ctmp.w = alpha * accum[m][n + 3] + beta * ctmp.w;
+                ctmp.x = alpha * c_frag[m * TN + n] + beta * ctmp.x;
+                ctmp.y = alpha * c_frag[m * TN + n + 1] + beta * ctmp.y;
+                ctmp.z = alpha * c_frag[m * TN + n + 2] + beta * ctmp.z;
+                ctmp.w = alpha * c_frag[m * TN + n + 3] + beta * ctmp.w;
                 FETCH_FLOAT4(C[OFFSET(ty + m, tx + n, N)]) = ctmp;
             }
         }
     }
 
-//    template<
-//            const unsigned int BM,
-//            const unsigned int BN,
-//            const unsigned int BK,
-//            const unsigned int WM,
-//            const unsigned int WN,
-//            const unsigned int WK,
-//            const unsigned int TM,
-//            const unsigned int TN
-//    >
-//    __global__ void warp_tiling(
-//            int M, int N, int K,
-//            float alpha,
-//            float *A,
-//            float *B,
-//            float beta,
-//            float *C
-//    ) {
-//        const unsigned int bx = blockIdx.x;
-//        const unsigned int by = blockIdx.y;
-//
-////        const uint K10_NUM_THREADS = 128;
-////        const uint K10_BN = 128;
-////        const uint K10_BM = 128;
-////        const uint K10_BK = 16;
-////        const uint K10_WN = 64;
-////        const uint K10_WM = 64;
-////        const uint K10_WNITER = 4;
-////        const uint K10_TN = 4;
-////        const uint K10_TM = 8;
-////        dim3 blockDim(K10_NUM_THREADS);
-//
-//    }
+    template<
+            const unsigned int BM,
+            const unsigned int BN,
+            const unsigned int BK,
+            const unsigned int TM,
+            const unsigned int TN
+    >
+    __global__ void warp_tiling(
+            int M, int N, int K,
+            float alpha,
+            float *A,
+            float *B,
+            float beta,
+            float *C
+    ) {
+
+    }
+
 }
 
 #endif //CUDA_KERNEL_MATMUL_CUH
